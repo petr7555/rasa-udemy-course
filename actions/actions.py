91daf0c6a550657d1dfb4ea764574299c839f60e
Petr Janik <peta.janik@email.cz>
2021-02-23 19:03:35 +0100

    Complete Forms

--- actions/actions.py
+++ actions/actions.py
@@ -25,3 +25,28 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+from typing import Text, List, Optional
+
+from rasa_sdk import Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormValidationAction
+
+
+class ValidateRestaurantForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_health_form"
+
+    async def required_slots(
+            self,
+            slots_mapped_in_domain: List[Text],
+            dispatcher: "CollectingDispatcher",
+            tracker: "Tracker",
+            domain: "DomainDict",
+    ) -> Optional[List[Text]]:
+        print(f"confirm_exercise is {tracker.get_slot('confirm_exercise')}")
+        if not tracker.get_slot("confirm_exercise"):
+            slots_mapped_in_domain.remove("exercise")
+
+        print(slots_mapped_in_domain)
+        return slots_mapped_in_domain
