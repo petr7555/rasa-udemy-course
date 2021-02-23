489df260559a64602731d33b7a5a9f16d5ca47ca
Petr Janik <peta.janik@email.cz>
2021-02-23 22:31:18 +0100

    Complete Actions

---
+++ README.md
@@ -0,0 +1,7 @@
+In the project root, create `.env` file:
+
+```
+AIRTABLE_API_KEY=key*****
+BASE_ID=app*****
+TABLE_NAME=Table%201
+```
--- actions/actions.py
+++ actions/actions.py
@@ -26,11 +26,59 @@
 #
 #         return []
 
-from typing import Text, List, Optional
+from typing import Text, List, Optional, Dict, Any
 
-from rasa_sdk import Tracker
+from rasa_sdk import Tracker, Action
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormValidationAction
+from dotenv import load_dotenv
+import os
+import requests
+import json
+import uuid
+
+load_dotenv()
+
+airtable_api_key = os.getenv("AIRTABLE_API_KEY")
+base_id = os.getenv("BASE_ID")
+table_name = os.getenv("TABLE_NAME")
+
+
+def create_health_log(confirm_exercise, exercise, sleep, diet, stress, goal):
+    request_url = f"https://api.airtable.com/v0/{base_id}/{table_name}"
+
+    headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {airtable_api_key}",
+    }
+
+    data = {
+        "fields": {
+            "Id": str(uuid.uuid4()),
+            "Exercised?": confirm_exercise,
+            "Type of exercise": exercise,
+            "Amount of sleep": sleep,
+            "Stress": stress,
+            "Diet": diet,
+            "Goal": goal,
+        }
+    }
+
+    print(request_url)
+    print(headers)
+    print(json.dumps(data))
+
+    try:
+        response = requests.post(
+            request_url, headers=headers, data=json.dumps(data)
+        )
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        raise SystemExit(err)
+
+    print(response.status_code)
+    return response
 
 
 class ValidateRestaurantForm(FormValidationAction):
@@ -50,3 +98,25 @@ class ValidateRestaurantForm(FormValidationAction):
 
         print(slots_mapped_in_domain)
         return slots_mapped_in_domain
+
+
+class SubmitHealthForm(Action):
+
+    def name(self) -> Text:
+        return "submit_health_form"
+
+    async def run(
+            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        confirm_exercise = tracker.get_slot("confirm_exercise")
+        exercise = tracker.get_slot("exercise")
+        sleep = tracker.get_slot("sleep")
+        stress = tracker.get_slot("stress")
+        diet = tracker.get_slot("diet")
+        goal = tracker.get_slot("goal")
+
+        response = create_health_log(confirm_exercise, exercise, sleep, stress, diet, goal)
+
+        dispatcher.utter_message("Thanks, your answers have been recorded!")
+
+        return []
