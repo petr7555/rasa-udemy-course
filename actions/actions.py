933ffce97fb69ccc952e3653139a4ae95b8eb129
Petr Janik <peta.janik@email.cz>
2021-02-28 23:32:43 +0100

    Add missing type import

--- actions/actions.py
+++ actions/actions.py
@@ -31,6 +31,7 @@ from typing import Text, List, Optional, Dict, Any
 from rasa_sdk import Tracker, Action
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.types import DomainDict
 from dotenv import load_dotenv
 import os
 import requests
